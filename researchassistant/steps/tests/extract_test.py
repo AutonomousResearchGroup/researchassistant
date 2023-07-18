9afe1c904c7d3c475a67ac8f7da3461d03a71644
moon <shawmakesmagic@gmail.com>
2023-07-18 00:38:31 -0700

    cleanup

--- researchassistant/steps/tests/extract_test.py
+++ researchassistant/steps/tests/extract_test.py
@@ -1,12 +1,8 @@
 import sys
 
 from researchassistant.steps.b_extract import extract_from_file_or_url
-from researchassistant.helpers.topics import add_topics_to_memory, get_topics_from_csv
-
-add_topics_to_memory(get_topics_from_csv())
 
 research_topic = "I am researching existential risk and the societal impact of artificial intelligence. I am interested in anything related to AI, machine learning, and the future of humanity. I am also interested in the positive societal impacts of AI, including how it could impact other existential risks such as climate change and supply chain risks."
-
 
 urls=[
 "https://test-page-to-crawl.vercel.app/nolinks"
--- run_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 researchassistant/research_test.py
-python3 researchassistant/search_test.py
-python3 researchassistant/archive_test.py
